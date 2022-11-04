f1d5acd9d012d0fa6489be4b88b32d681030e461
Abdulmalik Yusuf <yusuffabdulmalik@gmail.com>
2022-11-04 19:01:49 +0100

    feat: implemented contact page& changed styling to use tailwind

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 
 function Contact() {
-  return (
-    <main>
-        <form>
+    const name = "Abdulmalik Yusuf"
+    return (
+        <main className="max-w-[720px] mx-auto">
             <div>
-                <label>First Name</label>
-                <input type="text" id="first_name" placeholder="" className=""/>
+                <h1 className="font-semibold text-[36px] leading-[44px] text-custom-gray-900 tracking-[-0.02em]">Contact Me</h1>
+                <p className="mt-5 text-xl leading-[30px] text-custom-gray-600">Hi there, contact me to ask me about anything you have in mind.</p>
             </div>
-            <div>
-                <button type="submit" id="btn__submit">Submit</button>
-            </div>
-        </form>
-    </main>
-  )
+            <form className="mt-12 space-y6 grid grid-cols-2 gap-6">
+                <div className="col-span-full md:col-span-1">
+                    <label className="text-sm mb-[6px] block">First Name</label>
+                    <input type="text" id="first_name" placeholder="Enter your first name" className="input__field"/>
+                </div>
+                <div className="col-span-full md:col-span-1">
+                    <label className="text-sm mb-[6px] block">Last Name</label>
+                    <input type="text" id="last_name" placeholder="Enter your last name" className="input__field"/>
+                </div>
+                <div className="col-span-full">
+                    <label className="text-sm mb-[6px] block">Email</label>
+                    <input type="text" id="email" placeholder="yourname@email.com" className="input__field"/>
+                </div>
+                <div className="col-span-full">
+                    <label className="text-sm mb-[6px] block">Message</label>
+                    <textarea rows={5} id="message" placeholder="Send me a message and I'll reply you as soon as possible..." className="input__field"/>
+                </div>
+                <div className="col-span-full flex items-center space-x-3">
+                    <input type="checkbox" className="h-5 w-5 border border-custom-gray-300 rounded-[6px]"/>
+                    <p className="text-custom-gray-600">You agree to providing your data to {name} who may contact you.</p> 
+                </div>
+                <div className="col-span-full">
+                    <button type="submit" id="btn__submit" className="submit__btn">Submit</button>
+                </div>
+            </form>
+        </main>
+    )
 }
 
 export default Contact
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,80 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
   content: [
     "./src/pages/*.{js,jsx,ts,tsx}",
     "./src/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray-900': '#101828',
+        'custom-gray-500': '#667085',
+        'custom-gray-400': '#98A2B3',
+        'custom-gray-300': '#D0D5DD',
+        'custom-gray-200': '#EAECF0',
+        'custom-gray-100': '#F2F4F7',
+        'custom-gray-50': '#F9FAFB',
+        'custom-gray-25': '#FCFCFD',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(function ({ addUtilities }) {
+			addUtilities({
+				".share__btn": {
+          height: '40px',
+          width: '40px',
+          'border-radius': '999%',
+          border: 'dashed 1.67px var(--gray-400)',
+          display: 'flex',
+          'justify-content': 'center',
+          'align-items': 'center',
+          /* position: 'absolute', */
+          right: '200px',
+          top: '-20px',
+          "&:hover": {
+            background: 'var(--gray-50)'
+          },
+          "&:focus": {
+            background: '#FFFFFF',
+            border: '1px dashed var(--gray-300)',
+            'box-shadow': '0px 0px 0px 4px var(--gray-100)',
+          },
+          "&:disabled": {
+            background: 'var(--gray-50)',
+            border: '1px dashed var(--gray-200)',
+          }
+        },
+        ".input__field":{
+          padding: '10px 14px',
+          width: '100%',
+          // height: '44px',
+          background: '#FFFFFF',
+          border: '1px solid #D0D5DD',
+          'box-shadow': '0px 1px 2px rgba(16, 24, 40, 0.05)',
+          'border-radius': '8px',
+        },
+        ".submit__btn": {
+          'margin-top': '34px',
+          display: 'flex',
+          'flex-direction': 'row',
+          'justify-content': 'center',
+          'align-items': 'center',
+          padding: '12px 20px',
+          // gap: '8px',
+          width: '100%',
+          // height: '48px',
+          background: '#1570EF',
+          border: '1px solid #1570EF',
+          'box-shadow': '0px 1px 2px rgba(16, 24, 40, 0.05)',
+          'border-radius': '8px',
+          color: 'white',
+          'font-weight': '700'
+        }
+      })
+    })
+  ],
 }
